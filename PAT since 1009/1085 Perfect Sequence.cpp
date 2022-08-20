53dfbf8a47d439a9a90c14fc4f9fb2a3edeef411
xiaofeng <1244762145@qq.com>
2022-08-20 16:37:25 +0800

    前序和中序求后序 自己struct建树求出来感觉逻辑没问题，但是三个测试点过不了，再接再厉

--- PAT since 1009/1085 Perfect Sequence.cpp
+++ PAT since 1009/1085 Perfect Sequence.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<string>
-#include<map>
-#include<vector>
-#include<algorithm>
-using namespace std;
-
-int main() {
-	long long p, n, num;
-	vector<int>v;
-	scanf("%d %d", &n, &p);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &num);
-		v.push_back(num);
-	}
-	sort(v.begin(), v.end(), less<int>());
-	int distance = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + distance; j < n; j++)
-		{
-			if (v[j] <= v[i] * p && v[i] * p > 0) {
-				int temp = j - i + 1;
-				if (temp > distance)distance = temp;
-			}
-			else {
-				break;
-			}
-		}
-	}
-	printf("%d", distance);
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include<map>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//int main() {
+//	long long p, n, num;
+//	vector<int>v;
+//	scanf("%d %d", &n, &p);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf("%d", &num);
+//		v.push_back(num);
+//	}
+//	sort(v.begin(), v.end(), less<int>());
+//	int distance = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = i + distance; j < n; j++)
+//		{
+//			if (v[j] <= v[i] * p && v[i] * p > 0) {
+//				int temp = j - i + 1;
+//				if (temp > distance)distance = temp;
+//			}
+//			else {
+//				break;
+//			}
+//		}
+//	}
+//	printf("%d", distance);
+//	return 0;
+//}
