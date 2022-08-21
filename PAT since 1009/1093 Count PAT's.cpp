5e2dbdde2962a2c40785354b6137932910d71bed
xiaofeng <1244762145@qq.com>
2022-08-21 16:14:55 +0800

    真不可思议

---
+++ PAT since 1009/1093 Count PAT's.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//#include<string>
+//#include<map>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//const int modNum = 1000000007;
+//
+//long long sum;
+//int main() {
+//	int t = 0, a = 0, p = 0;
+//	string pat;
+//	cin >> pat;
+//	for (int i = pat.length() - 1; i >= 0; i--)
+//	{
+//		if (pat[i] == 'T')t++;
+//		else if (pat[i] == 'A')a += t;
+//		else {
+//			sum += a;
+//		}
+//	}
+//	sum %= modNum;
+//	cout << sum;
+//	return 0;
+//}
