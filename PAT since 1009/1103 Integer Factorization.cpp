4950452345d510bcfe5dd485a0ea7ba1ed8ec52c
xiaofeng <1244762145@qq.com>
2022-08-28 11:46:37 +0800

    观察法，测试点2 涉及到double精度的问题 double类型的值多次累加导致的精度误差，因为输入为十进制小数，存储到double中时，计算机内部使用二进制表示，且计算机的字长有限，有的十进制浮点数使用二进制无法精确表示只能无限接近，在字长的限制下不可避免会产生舍入误差

--- PAT since 1009/1103 Integer Factorization.cpp
+++ PAT since 1009/1103 Integer Factorization.cpp
@@ -1,89 +1,89 @@
-#include<iostream>
-#include<string>
-#include<cmath>
-#include<vector>
-#include<algorithm>
-using namespace std;
-
-int n, k, p;
-vector<vector<int>>ans;
-vector<vector<int>>only;
-int record[25];
-void travel(vector<int>v, int num, int level) {
-	if (num == 0 || level == k) {
-		if (num == 0 && level == k)ans.push_back(v);
-		return;
-	}
-	int x = sqrt(num);
-	if (v.size() > 0) {
-		x = min(x, v[v.size() - 1]);
-	}
-	for (int i = x; i > 0; i--)
-	{
-		if (k * record[i] < num) {
-			return;
-		}
-		int len = v.size();
-		if (v.size() > 0 && i > v[len - 1]) {
-			return;
-		}
-		if (level + 1 <= k) {
-			v.push_back(i);
-			travel(v, num - record[i], level + 1);
-			v.pop_back();
-		}
-	}
-}
-
-int main() {
-	scanf_s("%d%d%d", &n, &k, &p);
-	vector<int>temp;
-	for (int i = 0; i < 25; i++)
-	{
-		record[i] = pow(i, p);
-	}
-	travel(temp, n, 0);
-	if (ans.size() == 0) {
-		printf("Impossible");
-		return 0;
-	}
-	int max = 0;
-	for (int i = 0; i < ans.size(); i++)
-	{
-		int sum = 0;
-		for (int x : ans[i])sum += x;
-		if (sum > max) {
-			max = sum;
-			only.clear();
-			only.push_back(ans[i]);
-		}
-		else if (sum == max)only.push_back(ans[i]);
-	}
-
-	int index = 0;
-	while (only.size() > 1 && index < k) {
-		int max = only[0][index];
-		ans.clear();
-		ans.push_back(only[0]);
-		for (int i = 0; i < only.size(); i++)
-		{
-			if (only[i][index] > max) {
-				ans.clear();
-				ans.push_back(only[i]);
-				max = only[i][index];
-			}
-			else if (only[i][index] == max) {
-				ans.push_back(only[i]);
-			}
-		}
-		only.clear();
-		only = ans;
-		index++;
-	}
-	printf("%d = %d^%d", n, only[0][0], p);
-	for (int i = 1; i < only[0].size(); i++)
-	{
-		printf(" + %d^%d", only[0][i], p);
-	}
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include<cmath>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//int n, k, p;
+//vector<vector<int>>ans;
+//vector<vector<int>>only;
+//int record[25];
+//void travel(vector<int>v, int num, int level) {
+//	if (num == 0 || level == k) {
+//		if (num == 0 && level == k)ans.push_back(v);
+//		return;
+//	}
+//	int x = sqrt(num);
+//	if (v.size() > 0) {
+//		x = min(x, v[v.size() - 1]);
+//	}
+//	for (int i = x; i > 0; i--)
+//	{
+//		if (k * record[i] < num) {
+//			return;
+//		}
+//		int len = v.size();
+//		if (v.size() > 0 && i > v[len - 1]) {
+//			return;
+//		}
+//		if (level + 1 <= k) {
+//			v.push_back(i);
+//			travel(v, num - record[i], level + 1);
+//			v.pop_back();
+//		}
+//	}
+//}
+//
+//int main() {
+//	scanf_s("%d%d%d", &n, &k, &p);
+//	vector<int>temp;
+//	for (int i = 0; i < 25; i++)
+//	{
+//		record[i] = pow(i, p);
+//	}
+//	travel(temp, n, 0);
+//	if (ans.size() == 0) {
+//		printf("Impossible");
+//		return 0;
+//	}
+//	int max = 0;
+//	for (int i = 0; i < ans.size(); i++)
+//	{
+//		int sum = 0;
+//		for (int x : ans[i])sum += x;
+//		if (sum > max) {
+//			max = sum;
+//			only.clear();
+//			only.push_back(ans[i]);
+//		}
+//		else if (sum == max)only.push_back(ans[i]);
+//	}
+//
+//	int index = 0;
+//	while (only.size() > 1 && index < k) {
+//		int max = only[0][index];
+//		ans.clear();
+//		ans.push_back(only[0]);
+//		for (int i = 0; i < only.size(); i++)
+//		{
+//			if (only[i][index] > max) {
+//				ans.clear();
+//				ans.push_back(only[i]);
+//				max = only[i][index];
+//			}
+//			else if (only[i][index] == max) {
+//				ans.push_back(only[i]);
+//			}
+//		}
+//		only.clear();
+//		only = ans;
+//		index++;
+//	}
+//	printf("%d = %d^%d", n, only[0][0], p);
+//	for (int i = 1; i < only[0].size(); i++)
+//	{
+//		printf(" + %d^%d", only[0][i], p);
+//	}
+//	return 0;
+//}
---
+++ PAT since 1009/1104 Sum of Number Segments.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+double a[100005];
+
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	long long sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		scanf_s("%lf", &a[i]);
+		sum += (long long)(a[i] * 1000) * i * (n + 1 - i);
+	}
+	printf("%.2f", sum / 1000.0);
+	return 0;
+}
