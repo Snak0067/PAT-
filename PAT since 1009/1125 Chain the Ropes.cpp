53a4d34dada6631ede6ae165e3bfc7c26645a39d
xiaofeng <1244762145@qq.com>
2022-09-02 15:26:34 +0800

    绳子打折，系尽可能长的绳子

---
+++ PAT since 1009/1125 Chain the Ropes.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//#include<string>
+//#include<map>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//int v[10005];
+//
+//int main() {
+//	int n, temp;
+//	scanf_s("%d", &n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		scanf_s("%d", &v[i]);
+//	}
+//	sort(v, v + n);
+//	temp = v[0];
+//	for (int i = 1; i < n; i++)
+//	{
+//		temp = (temp + v[i]) / 2;
+//	}
+//	printf("%d\n", temp);
+//	return 0;
+//}
