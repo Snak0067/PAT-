2590e64ba4bd35aed59bebe17421dc92175bfd0c
xiaofeng <1244762145@qq.com>
2022-09-10 16:48:52 +0800

    1142 AC

--- PAT since 1009/1141 PAT Ranking of Institutions.cpp
+++ PAT since 1009/1141 PAT Ranking of Institutions.cpp
@@ -1,75 +1,75 @@
-#include<iostream>
-#include<string>
-#include<unordered_map>
-#include<vector>
-#include<algorithm>
-using namespace std;
-
-struct node {
-	string school;
-	int rank, num = 0, tws = 0;
-	double score = 0;
-};
-unordered_map<string, int>mmap;
-vector<node>v;
-bool cmp(node a, node b) {
-	if (a.tws != b.tws)return a.tws > b.tws;
-	if (a.num != b.num)return a.num < b.num;
-	return a.school < b.school;
-}
-int main() {
-	string id, institution;
-	int	n, index = 0;
-	double score;
-	cin >> n;
-	v.resize(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> id;
-		scanf("%lf", &score);
-		cin >> institution;
-		transform(institution.begin(), institution.end(), institution.begin(), ::tolower);
-		if (mmap.find(institution) == mmap.end()) {
-			mmap[institution] = index++;
-			v[mmap[institution]].school = institution;
-		}
-		v[mmap[institution]].num++;
-		switch (id[0])
-		{
-		case 'A':
-			v[mmap[institution]].score += score;
-			break;
-		case 'B':
-			v[mmap[institution]].score += (score / 1.5);
-			break;
-		case 'T':
-			v[mmap[institution]].score += (score * 1.5);
-			break;
-		default:
-			break;
-		}
-	}
-	for (int i = 0; i < index; i++)
-	{
-		v[i].tws = (int)(v[i].score);
-	}
-	sort(v.begin(), v.end(), cmp);
-	printf("%d\n", index);
-	for (int i = 0; i < index; i++)
-	{
-		if (i == 0) {
-			printf("1 %s %d %d\n", v[i].school.c_str(), v[i].tws, v[i].num);
-			v[i].rank = 1;
-		}
-		else if (v[i].tws == v[i - 1].tws) {
-			v[i].rank = v[i - 1].rank;
-			printf("%d %s %d %d\n", v[i].rank, v[i].school.c_str(), v[i].tws, v[i].num);
-		}
-		else {
-			v[i].rank = i + 1;
-			printf("%d %s %d %d\n", v[i].rank, v[i].school.c_str(), v[i].tws, v[i].num);
-		}
-	}
-
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//#include<unordered_map>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//struct node {
+//	string school;
+//	int rank, num = 0, tws = 0;
+//	double score = 0;
+//};
+//unordered_map<string, int>mmap;
+//vector<node>v;
+//bool cmp(node a, node b) {
+//	if (a.tws != b.tws)return a.tws > b.tws;
+//	if (a.num != b.num)return a.num < b.num;
+//	return a.school < b.school;
+//}
+//int main() {
+//	string id, institution;
+//	int	n, index = 0;
+//	double score;
+//	cin >> n;
+//	v.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> id;
+//		scanf("%lf", &score);
+//		cin >> institution;
+//		transform(institution.begin(), institution.end(), institution.begin(), ::tolower);
+//		if (mmap.find(institution) == mmap.end()) {
+//			mmap[institution] = index++;
+//			v[mmap[institution]].school = institution;
+//		}
+//		v[mmap[institution]].num++;
+//		switch (id[0])
+//		{
+//		case 'A':
+//			v[mmap[institution]].score += score;
+//			break;
+//		case 'B':
+//			v[mmap[institution]].score += (score / 1.5);
+//			break;
+//		case 'T':
+//			v[mmap[institution]].score += (score * 1.5);
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//	for (int i = 0; i < index; i++)
+//	{
+//		v[i].tws = (int)(v[i].score);
+//	}
+//	sort(v.begin(), v.end(), cmp);
+//	printf("%d\n", index);
+//	for (int i = 0; i < index; i++)
+//	{
+//		if (i == 0) {
+//			printf("1 %s %d %d\n", v[i].school.c_str(), v[i].tws, v[i].num);
+//			v[i].rank = 1;
+//		}
+//		else if (v[i].tws == v[i - 1].tws) {
+//			v[i].rank = v[i - 1].rank;
+//			printf("%d %s %d %d\n", v[i].rank, v[i].school.c_str(), v[i].tws, v[i].num);
+//		}
+//		else {
+//			v[i].rank = i + 1;
+//			printf("%d %s %d %d\n", v[i].rank, v[i].school.c_str(), v[i].tws, v[i].num);
+//		}
+//	}
+//
+//	return 0;
+//}
---
+++ PAT since 1009/1142 Maximal Clique.cpp
@@ -0,0 +1,60 @@
+//#include<iostream>
+//#include<string>
+//#include<unordered_map>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//
+//vector<int>vec[205];
+//
+//int main() {
+//	int n, k, m, a, b;
+//	scanf_s("%d%d", &n, &k);
+//	for (int i = 0; i < k; i++)
+//	{
+//		scanf_s("%d%d", &a, &b);
+//		vec[a].push_back(b);
+//		vec[b].push_back(a);
+//	}
+//	scanf_s("%d", &m);
+//	for (int i = 0; i < m; i++)
+//	{
+//		scanf_s("%d", &k);
+//		unordered_map<int, int>mmap;
+//		vector<int>temp;
+//		for (int u = 0; u < k; u++)
+//		{
+//			scanf_s("%d", &a);
+//			temp.push_back(a);
+//			for (int j = 0; j < vec[a].size(); j++)
+//			{
+//				mmap[vec[a][j]]++;
+//			}
+//		}
+//		auto iter = temp.begin();
+//		while (iter != temp.end()) {
+//			if (mmap[*iter] < k - 1) {
+//				printf("Not a Clique\n");
+//				break;
+//			}
+//			iter++;
+//		}
+//		if (iter == temp.end()) {
+//			int count = 0;
+//			auto it = mmap.begin();
+//			while (it != mmap.end()) {
+//				if (it->second >= k) {
+//					printf("Not Maximal\n");
+//					break;
+//				}
+//				it++;
+//			}
+//			if (it == mmap.end()) {
+//				printf("Yes\n");
+//			}
+//		}
+//	}
+//
+//	return 0;
+//}
